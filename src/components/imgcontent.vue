dd5b2eed8b9f2ff518cb9f9595395c8793195a36
ly2011 <1518550424@qq.com>
2023-12-10 23:20:40 +0800

    feat: el-image src 来源改为 base64 文件

--- src/components/imgcontent.vue
+++ src/components/imgcontent.vue
@@ -4,6 +4,11 @@
 </template>
 
 <script>
+import imagesBase64 from "./imagesBase64.json";
+const img1 = imagesBase64[0].base64;
+const img2 = imagesBase64[1].base64;
+const img3 = imagesBase64[2].base64;
+console.warn(img1);
 export default {
   data() {
     return {
@@ -29,7 +34,7 @@ export default {
   },
   beforeCreate() {
     this.content = `
-            <p><img width="100%" height="100%" src="https://cdn.pixabay.com/photo/2014/11/30/14/11/cat-551554_1280.jpg" alt="image"><div style="display: flex; justify-content: center; flex-direction: column;"><h3 style="text-align: center;">我是大图，需要装</h3><span style="text-align: center;">我是描述语</span></div><img width="100%" height="100%" src="https://cdn.pixabay.com/photo/2014/11/30/14/11/cat-551554_1280.jpg" alt="image"><div style="display: flex; justify-content: center; flex-direction: column;"><h3 style="text-align: center;">我是大图，需要装</h3><span style="text-align:center;">我是描述语</span></div><img width="100%" height="100%" src="https://cdn.pixabay.com/photo/2014/11/30/14/11/cat-551554_1280.jpg" alt="image"><div style="display: flex; justify-content: center; flex-direction: column;"><h3 style="text-align: center;">我是大图，需要装</h3><span style="text-align: center;">我是描述语</span></div></p>
+            <p><img width="100%" height="100%" src="${img1}" alt="image"><div style="display: flex; justify-content: center; flex-direction: column;"><h3 style="text-align: center;">我是大图，需要装</h3><span style="text-align: center;">我是描述语</span></div><img width="100%" height="100%" src="${img2}" alt="image"><div style="display: flex; justify-content: center; flex-direction: column;"><h3 style="text-align: center;">我是大图，需要装</h3><span style="text-align:center;">我是描述语</span></div><img width="100%" height="100%" src="${img3}" alt="image"><div style="display: flex; justify-content: center; flex-direction: column;"><h3 style="text-align: center;">我是大图，需要装</h3><span style="text-align: center;">我是描述语</span></div></p>
             `;
     this.content = this.content.replace(
       /<img (width="100%" height="100%") src="(.*?)" alt="(.*?)">/g,
