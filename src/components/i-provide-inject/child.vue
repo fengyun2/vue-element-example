4b628e65314a0b091b889eb5a7668fb03f862d5e
ly2011 <1518550424@qq.com>
2023-12-30 11:13:11 +0800

    feat: 添加provide / inject demo

---
+++ src/components/i-provide-inject/child.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="page">
+    <h4>child</h4>
+    <p>不可响应的数据--msg: {{ msg }}</p>
+    <p>可响应的数据--activeData: {{ activeData.msg || "" }}</p>
+    <el-button @click="printDetail">打印</el-button>
+    <p>来自父级的内容--text: {{ detailVm.text }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "child",
+  inject: ["msg", "activeData", "detailVm"],
+  mounted() {
+    console.log("child mounted");
+  },
+  methods: {
+    printDetail() {
+      console.log(this.detailVm.text); // 获取detail中的变量;
+      this.detailVm.text = "对不起我在子组件中修改了";
+      this.detailVm.say(); // 调用detail中的方法;
+    },
+  },
+};
+</script>
+
+<style></style>
---
+++ src/components/i-provide-inject/index.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="page">
+    <h3>provide / inject</h3>
+    <p>
+      <el-input
+        v-model="msg"
+        placeholder="请输入内容"
+        @change="handleChangeMsg"
+      ></el-input>
+      <Child />
+    </p>
+  </div>
+</template>
+
+<script>
+// [你可能不知道的provide / inject 用法](https://segmentfault.com/a/1190000020954324)
+import Child from "./child.vue";
+export default {
+  name: "i-provide-inject",
+  components: {
+    Child,
+  },
+  provide() {
+    return {
+      msg: this.msg,
+      activeData: this.activeData,
+      detailVm: this, // 传递父级
+    };
+  },
+  data() {
+    return {
+      msg: "hello world",
+      activeData: {
+        msg: "hello world",
+      },
+      text: "say hello",
+    };
+  },
+  methods: {
+    handleChangeMsg(value) {
+      this.activeData.msg = value;
+    },
+    say() {
+      console.log(this.text);
+    },
+  },
+};
+</script>
+
+<style></style>
